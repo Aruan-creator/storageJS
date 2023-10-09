b57b08bf54b1bf258c9fc05c2178c623a673776c
Ryan <aruan.kot3@gmail.com>
2023-10-09 20:31:39 +0600

    added schema

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { createClient } = require('redis');  
+const http = require('node:http');
+
+const server = http.createServer((req, res) => {
+});
+
+const getPriceByArticul = () => {
+    //request for redis
+}
+
+const goodsSchema = {
+    'vendor': '',
+    'articul': '',
+    'date': '',
+    'price': 0,
+    'quantity': 0
+}
+
+const main = async() => {
+    const client = createClient();  
+    let userSession = await client.hGetAll('user-session:123');
+    console.log(JSON.stringify(userSession, null, 2));
+    server.listen(8000);
+};
+main();
