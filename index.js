b3514b6d54958f8962786e8d97ebe7989ccbc723
Aruan-creator <aruan.kot3@gmail.com>
2023-10-10 07:39:25 +0600

    test commit

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const goodsSchema = {
     'quantity': 0
 }
 
+const testF = () => {};
+
 const main = async() => {
     const client = createClient();  
     let userSession = await client.hGetAll('user-session:123');
