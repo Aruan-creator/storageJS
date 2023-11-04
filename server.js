fe866ac76d0e42ed57fcdfa0e49dc86c07ec21a3
Aruan-creator <aruan.kot3@gmail.com>
2023-11-04 16:53:51 +0600

    Database migration

---
+++ server.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const fs = require('node:fs');
+const http = require('node:http');
+
+const api = new Map();
+
+const apiPath = './api';
+
+const cacheFile = (name) => {
+    const filePath = apiPath + name;
+    const key = path.basename(filePath, '.js');
+    try {
+        const libPath = require.resolve(filePath);
+        delete require.cache[libPath];     
+    } catch (e) {
+        return;
+    }
+    try {
+        const method = require(filePath);
+        api.set(key, method);
+    } catch (e) {
+        api.delete(key);
+    }
+};
+
+const cacheFolder = (path) => {
+    fs.readdir(path, (err, files) => {
+        if (err) return;
+        files.forEach(cacheFile);
+    });
+};
+
+cacheFolder(apiPath);
+fs.watch(apiPath);
+
+setTimeout(() => {
+    console.dir({ api });
+}, 1000);
+
+const recieveArgs = async (req) => {
+    const buffers = [];
+    for await (const chunk of req) buffers.push(chunk);
+    const data = Buffer.concat(buffers).toString();
+    return JSON.parse(data);
+};
+
+const httpError = (res, status, message) => {
+    res.statusCode = status;
+    res.end(`"${message}"`);
+};
+
+http.createServer(async (req, res) => {
+    const url = req.url === '/' ? 'index.html' : req.url;
+    const [first, second] = url.substring(1).split('/');
+    if (first === 'api') {
+        const method = api.get(second);
+        const args = await recieveArgs(req);
+        try {
+            const result = await method(...args);
+            if (!result) {
+                httpError(res, 500, 'Server error');
+                return;
+            }
+            res.end(JSON.stringify(result));
+        } catch (err) {
+            console.dir({ err });
+            httpError(res, 500, 'Server error');
+        }
+    } else {
+        httpError(res, 404, 'File is not found');
+    }
+});
